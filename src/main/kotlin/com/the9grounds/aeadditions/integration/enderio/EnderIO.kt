8735f2176eb319c76edea50bb0ac0bdb01546a53
MasterYodA <aylon@the9grounds.com>
2021-07-10 19:15:33 -0500

    Only handle playerInteract for ender io workaround when it's enabled
    Fixes #22

--- src/main/kotlin/com/the9grounds/aeadditions/integration/enderio/EnderIO.kt
+++ src/main/kotlin/com/the9grounds/aeadditions/integration/enderio/EnderIO.kt
@@ -1,10 +1,52 @@
 package com.the9grounds.aeadditions.integration.enderio
 
 import com.the9grounds.aeadditions.api.AEAApi
+import com.the9grounds.aeadditions.block.BlockEC
+import crazypants.enderio.api.tool.ITool
+import net.minecraft.util.EnumHand
+import net.minecraftforge.event.entity.player.PlayerInteractEvent
+import net.minecraftforge.fml.common.eventhandler.Event
 
 object EnderIO {
 
     @JvmStatic fun init() {
         AEAApi.instance().registerWrenchHandler(WrenchHandler)
+    }
+
+    @JvmStatic fun handlePlayerInteract(event: PlayerInteractEvent.RightClickBlock) {
+        if (event.hand != EnumHand.MAIN_HAND) {
+            return
+        }
+
+        val blockState = event.world.getBlockState(event.pos)
+        val block = blockState.block
+        val itemStack = event.itemStack
+
+        val item = itemStack.item
+
+        if (item !is ITool || block !is BlockEC || event.face == null) {
+            return
+        }
+
+        if (!event.entityPlayer.isSneaking) {
+            return
+        }
+
+        val vec3d = event.hitVec
+
+        try {
+            val retVal = block.onBlockActivated(
+                event.world, event.pos, blockState, event.entityPlayer, event.hand, event.face,
+                vec3d.x.toFloat(),
+                vec3d.y.toFloat(),
+                vec3d.z.toFloat()
+            )
+            if (retVal) {
+                event.result = Event.Result.DENY
+                event.entityPlayer.swingArm(event.hand)
+            }
+        } catch (e: Exception) {
+            // Catch all exceptions
+        }
     }
 }
