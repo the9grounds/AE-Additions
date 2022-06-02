74f0fc21c2bf6ddaa856001837252895d3b52547
MasterYodA <yoda@the9grounds.com>
2022-06-02 12:52:24 -0500

    Separate server safe and client safe calls with mixin

--- src/main/kotlin/com/the9grounds/aeadditions/core/mixin/CraftingCubeModelMixin.java
+++ src/main/kotlin/com/the9grounds/aeadditions/core/mixin/CraftingCubeModelMixin.java
@@ -2,7 +2,7 @@ package com.the9grounds.aeadditions.core.mixin;
 
 import appeng.block.crafting.AbstractCraftingUnitBlock;
 import appeng.client.render.crafting.CraftingCubeModel;
-import com.the9grounds.aeadditions.util.AE2;
+import com.the9grounds.aeadditions.util.AE2Client;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.Material;
@@ -39,11 +39,11 @@ public abstract class CraftingCubeModelMixin {
     @Inject(method = "bake(Lnet/minecraft/client/resources/model/ModelBakery;Ljava/util/function/Function;Lnet/minecraft/client/resources/model/ModelState;Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/client/resources/model/BakedModel;", at = @At("HEAD"), cancellable = true, remap = true)
     private void bakeExtraCpus(ModelBakery bakery,
                                Function<Material, TextureAtlasSprite> spriteGetter, ModelState modelTransform, ResourceLocation modelLocation, CallbackInfoReturnable<BakedModel> callbackInfoReturnable) {
-        AE2.bakeExtraCpus(type, LIGHT_BASE, RING_CORNER, RING_SIDE_HOR, RING_SIDE_VER, spriteGetter, callbackInfoReturnable);
+        AE2Client.bakeExtraCpus(type, LIGHT_BASE, RING_CORNER, RING_SIDE_HOR, RING_SIDE_VER, spriteGetter, callbackInfoReturnable);
     }
     
     @Inject(method = "getAdditionalTextures()Ljava/util/stream/Stream;", at = @At("RETURN"), cancellable = true, remap = false)
     private void returnExtraCpuTextures(CallbackInfoReturnable<Stream<Material>> callbackInfoReturnable) {
-        callbackInfoReturnable.setReturnValue(AE2.combineTextures(callbackInfoReturnable.getReturnValue()));
+        callbackInfoReturnable.setReturnValue(AE2Client.combineTextures(callbackInfoReturnable.getReturnValue()));
     }
 }
