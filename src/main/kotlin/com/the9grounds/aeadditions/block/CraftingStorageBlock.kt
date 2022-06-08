f820b427c5983a95d570142183b3d937b230dd9b
MasterYodA <yoda@the9grounds.com>
2022-06-07 22:00:21 -0500

    Somehow got left out

--- src/main/kotlin/com/the9grounds/aeadditions/block/CraftingStorageBlock.kt
+++ src/main/kotlin/com/the9grounds/aeadditions/block/CraftingStorageBlock.kt
@@ -1,20 +1,22 @@
 package com.the9grounds.aeadditions.block
 
-import appeng.block.crafting.CraftingStorageBlock
-import appeng.blockentity.crafting.CraftingStorageBlockEntity
+import appeng.block.crafting.AbstractCraftingUnitBlock
+import appeng.blockentity.crafting.CraftingBlockEntity
+import com.the9grounds.aeadditions.block.crafting.ExtendedCraftingUnitType
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Registry
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.block.entity.BlockEntityType.Builder
+import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.block.state.BlockState
 
 
-class CraftingStorageBlock(props: Properties?, type: CraftingUnitType?) : CraftingStorageBlock(props, type) {
+class CraftingStorageBlock(props: BlockBehaviour.Properties, type: ExtendedCraftingUnitType?) : AbstractCraftingUnitBlock<CraftingBlockEntity>(props, type) {
     init {
         val supplier = { blockPos: BlockPos, blockState: BlockState ->
-            CraftingStorageBlockEntity(Registry.BLOCK_ENTITY_TYPE.get(ResourceLocation("ae2", "crafting_storage")), blockPos, blockState)
+            CraftingBlockEntity(Registry.BLOCK_ENTITY_TYPE.get(ResourceLocation("ae2", "crafting_storage")), blockPos, blockState)
         }
         val type = Builder.of(supplier, this).build(null)
-        setBlockEntity(CraftingStorageBlockEntity::class.java, type, null, null)
+        setBlockEntity(CraftingBlockEntity::class.java, type, null, null)
     }
 }
